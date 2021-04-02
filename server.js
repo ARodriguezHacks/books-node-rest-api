0db123f707cf7519f6284354ec3ee2fb75db6abd
Angie Rodriguez <angrodri902@gmail.com>
2021-04-02 14:53:05 -0700

    Initial express server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () =>
+  console.log(`Hello world app is listening on port ${port}`)
+);
